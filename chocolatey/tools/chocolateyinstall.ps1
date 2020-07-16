81f44c938e4fa1a42977d2ac64ca9b65ccad1210
Greg Z <gzarkis@gmail.com>
2020-07-16 13:29:10 -0400

    Feature/oops 4615/create distribution package (#2)
    * Formatting updates - still working on nuget packages and references
    
    * Adding inlined Plex formatter
    
    * Temp add of files that have dependencies configured in the packages project files.... this should eventually be removed completely by emptying the packages folder from the repo. testing
    
    * Adding nuget packages to replace assumed existing assemblies
    
    * Updated gitignore to filter out the packages
    
    * Cleanup and addition of Plex specific solution file to remove unnecessary projects
    
    * Adding Nuke project
    
    * Chocolatey package support
    
    * Clean up
    
    * Added helper scripts to locate VSIX installer - borrowed from https://github.com/jberezanski/ChocolateyPackages/blob/master/chocolatey-visualstudio.extension
    
    * Removing unnecessary chocolatey files as well as the pipeline yml
    
    Co-authored-by: Greg Zarkis <gzarkis@plex.com>

---
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,41 @@
+﻿## Install MSI
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'PoorMansTSqlFormatterSSMSPackage.Setup.msi'
+$checksumPathMsi = Join-Path $toolsDir 'PoorMansTSqlFormatterSSMSPackage.Setup.msi.hash'
+$msichecksum = [IO.File]::ReadAllText($checksumPathMsi)
+# $checksumPathVsix = Join-Path $toolsDir 'PoorMansTSqlFormatterVSPackage2019.vsix.hash'
+# $vsixChecksum = [IO.File]::ReadAllText($checksumPathVsix)
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI' 
+  file          = $fileLocation
+  softwareName  = 'PoorMansTSqlFormatter.Plex*'
+  checksum      = $msichecksum
+  checksumType  = 'md5'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)  
+}
+Install-ChocolateyInstallPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-install-package
+
+. .\tools\Get-VisualStudioVsixInstaller.ps1
+. .\tools\Get-WillowInstalledProducts.ps1
+$vsixInstaller = Get-VisualStudioVsixInstaller -Latest
+Write-Verbose ('Found VSIXInstaller version {0}: {1}' -f $vsixInstaller.Version, $vsixInstaller.Path)
+$installer = $vsixInstaller.Path
+
+$vsix = Join-Path $toolsDir 'PoorMansTSqlFormatterVSPackage2019.vsix'
+# The Install-ChocolateyVsixPackage cmdlet below can't ever seem to locate the install location so we're punting with the function above to locate vsixInstaller
+# Install-ChocolateyVsixPackage -PackageName "PoorMansTSqlFormatterVSPackage2019" $vsix -VsVersion 16
+$exitCode = Write-Host "Installing $vsix using $installer" 
+$psi = New-Object System.Diagnostics.ProcessStartInfo
+$psi.FileName=$installer
+$psi.Arguments= $vsix
+$s = [System.Diagnostics.Process]::Start($psi)
+$s.WaitForExit()
+$exitCode = $s.ExitCode
+if($exitCode -gt 0 -and $exitCode -ne 1001) { #1001: Already installed
+  throw "There was an error installing '$packageName'. The exit code returned was $exitCode."
+}
---
+++ chocolatey/tools/chocolateyuninstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+# vsixinstaller /u:5e84b709-1e60-4116-a702-4cdb1a282d6e
+. .\tools\Get-VisualStudioVsixInstaller.ps1
+. .\tools\Get-WillowInstalledProducts.ps1
+$vsixInstaller = Get-VisualStudioVsixInstaller -Latest
+Write-Verbose ('Found VSIXInstaller version {0}: {1}' -f $vsixInstaller.Version, $vsixInstaller.Path)
+$installer = $vsixInstaller.Path
+$psi = New-Object System.Diagnostics.ProcessStartInfo
+$psi.FileName=$installer
+$psi.Arguments="/u:5e84b709-1e60-4116-a702-4cdb1a282d6e"
+$s = [System.Diagnostics.Process]::Start($psi)
+$s.WaitForExit()
+$exitCode = $s.ExitCode
+if($exitCode -gt 0) { 
+  throw "There was an error uninstalling VSIX. The exit code returned was $exitCode."
+}
